699a4e1b4e6cb8a4cff112ecd9f50e3dad545d7c
Oskar Sęk <oskar.sek94@gmail.com>
2025-11-11 21:14:58 +0100

    feat: Introduce OpenAPI support and refactor authentication logic
    - Add OpenAPI definitions for `auth` and `cia-management` APIs.
    - Refactor `AuthController` to implement generated API interfaces.
    - Update `AuthService` and related components for better separation of concerns and input validation.
    - Enhance build.gradle with OpenAPI generator tasks and dependencies.

--- src/main/java/org/evoting/cia/domain/auth/model/FullLoginRequest.java
+++ src/main/java/org/evoting/cia/domain/auth/model/FullLoginRequest.java
@@ -4,6 +4,7 @@ import io.micronaut.core.annotation.Introspected;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
+import org.evoting.api.cia.model.LoginRequestDto;
 
 @Introspected
 @RequiredArgsConstructor
@@ -11,24 +12,7 @@ import lombok.Value;
 @Value
 public class FullLoginRequest {
 
-    String username;
-    String password;
-    String realm;
+    LoginRequestDto request;
     String host;
-
-    public static FullLoginRequest of(
-        LoginRequest lr,
-        String host,
-        String realm
-    ) {
-        if (lr == null) {
-            return null;
-        }
-        return FullLoginRequest.builder()
-            .username(lr.username())
-            .password(lr.password())
-            .host(host)
-            .realm(realm)
-            .build();
-    }
+    String realm;
 }
